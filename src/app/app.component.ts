6a92066889ceab088ecff54d9668c50e3df89f57
Matteo Merzi <matteo.merzi@gmail.com>
2017-01-19 22:54:40 +0100

    Initial project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'rb-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'rb-root',
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'rb works!';
+
 }
